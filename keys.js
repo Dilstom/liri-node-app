101481a7267a6de312efb6e5573d7bc798b21157
Dil Sim <dilyasom@gmail.com>
2017-09-13 18:25:55 -0700

    added changes

---
+++ keys.js
@@ -0,0 +1,25 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'VIHCwp9PqLqRs5PaZy3afiG08',
+  consumer_secret: 'RS8DUYBMiPF0kb59SIJN83VcPRZv76s9kcfTrZK7QYFD6HmhdF',
+  access_token_key: '907284424844124161-GxpM6louXepLaoNyD4NAbgkpnf5WC0T',
+  access_token_secret: 'ir2lZtOXSASIBkXoSq57FM7nF4VTBUBrCsn6LH2RpqID1',
+}
+
+
+
+// credentials are optional
+var spotifyApi = {
+ clientId : 'd896e695a9884bd3b3e092271396fe8b',
+ clientSecret : '0f17051fefa24e3e9d1c617da6dfaff4'
+};
+
+module.exports.twitterKeys = twitterKeys;
+module.exports.spotifyKeys = spotifyApi;
+ 
+//Spotify:
+// Client ID
+// d896e695a9884bd3b3e092271396fe8b
+// Client Secret
+// 0f17051fefa24e3e9d1c617da6dfaff4
---
+++ liri.js
@@ -0,0 +1,139 @@
+
+// grab keys
+const keys = require("./keys");
+
+var twitterKeys = keys.twitterKeys;
+var spotifyKeys = keys.spotifyKeys;
+
+// Load the fs package to read and write
+var Spotify = require('node-spotify-api');
+// var SpotifyWebApi = require('spotify-web-api-node');
+
+// credentials are optional
+var spotify = new Spotify({
+ id : spotifyKeys.clientId,
+ secret : spotifyKeys.clientSecret
+ });
+
+var fs = require("fs");
+var Twitter = require('twitter');
+var request = require("request");
+var action = process.argv[2];
+var song = process.argv[3];
+
+console.log(process.argv);
+processCommand(action)
+
+
+// will then create a switch-case statement (if-then would also work).
+// The switch-case will direct which function gets run.
+function processCommand(action) {
+  switch (action) {
+      case "my-tweets":
+      myTweets();
+        break;
+    
+      case "spotify-this-song":
+      spotifyThisSong();
+        break;
+    
+      case "movie-this":
+      movieThis();
+        break;
+    
+      case "do-what-it-says":
+      doWhatSays();
+        break;
+    }
+}
+// First function - myTweets()
+function myTweets() {
+   
+  var client = new Twitter({
+    consumer_key: twitterKeys.consumer_key,
+    consumer_secret: twitterKeys.consumer_secret,
+    access_token_key: twitterKeys.access_token_key,
+    access_token_secret: twitterKeys.access_token_secret
+  });
+
+  var params = {SunLand76912242: 'nodejs', count: 20};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+
+      for(i=0; i<tweets.length; i++) {
+        console.log(tweets[i].created_at)
+        console.log(tweets[i].text)
+      }
+      // console.log(tweets);
+    }
+  });
+}
+
+// Second function - spotifyThisSong()
+function spotifyThisSong() {
+  console.log(song)
+  if (!song) {
+    song = "The Sign"
+    console.log(song)
+}
+  spotify.search({ type: 'track', query: song }, function(err, thisSong) {
+   if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }else {
+      console.log("Artists/Band name: " + thisSong.tracks.items[0].album.artists[0].name)
+      console.log("Album name: " + thisSong.tracks.items[0].album.name)
+      console.log("Song's name: " + thisSong.tracks.items[0].name)
+      console.log("A preview link of the song: " + thisSong.tracks.items[0].external_urls.spotify)
+    }
+  });
+};
+
+function movieThis() {
+  // Grab the movieName which will always be the third node argument.
+  var movieName = process.argv[3];
+  // Then run a request to the OMDB API with the movie specified
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+  // This line is just to help us debug against the actual URL.
+  console.log(queryUrl);
+  if(movieName === "") {
+    console.log("Mr. Nobody")
+  }
+  request(queryUrl, function(error, response, body) {
+  // If the request is successful
+    if (!error && response.statusCode === 200) {
+      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+      var bodyObject = JSON.parse(body);
+      // console.log(bodyObject);
+      console.log("Movie Title: " + bodyObject.Title);
+      console.log("  Year: " + bodyObject.Year);
+      console.log("  IMDB Rating: " + bodyObject.imdbRating);
+      console.log("  Rotten Tomatoes Value: " + bodyObject.Ratings[1].Value);
+      console.log("  Country: " + bodyObject.Country);
+      console.log("  Language: " + bodyObject.Language);
+      console.log("  Plot: " + bodyObject.Plot);
+      console.log("  Actors: " + bodyObject.Actors);
+   }
+  });
+}
+
+// // last function
+  function doWhatSays() {
+  fs.readFile("random.txt", "utf8", function(err, data) {
+    if (err) {
+      return console.log(err);
+    } else {
+      // split data into 2 arguments
+      // argument 1 is command
+      // argument 2 is song
+
+      var args = data.split(',')
+      song = args[1]
+      action = args[0]
+
+      // pass these arguments to processCommand
+      processCommand(action)   
+
+        }
+});
+  }
